443de63930809c34937542cb951571bc1f44aa1a
finishahobby <138727608+finishahobby@users.noreply.github.com>
2023-07-05 17:58:38 -0400

    Add GPS to casestation.js

--- casestation.js
+++ casestation.js
@@ -10,163 +10,203 @@
     switch(station) {
       case "E1":  
         MartaStation=" Georgia State ";
-        StationURL = "https://itsmarta.com/Georgia-State.aspx";   
+        StationURL = "https://itsmarta.com/Georgia-State.aspx";
+        StationGPS = "";
         break;
       case "E2":  
         MartaStation=" King Memorial ";
         StationURL = "https://itsmarta.com/King-Memorial.aspx";
+        StationGPS = "";
         break;
       case "E3":  
         MartaStation=" Inman Park / Reynoldstown ";
         StationURL = "https://itsmarta.com/Inman-Park.aspx";
+        StationGPS = "";
         break;
       case "E4":  
         MartaStation=" Edgewood / Candler Park ";
         StationURL = "https://itsmarta.com/Edgewood-Candler-Park.aspx";
+        StationGPS = "33.76196,-84.33961";
         break;
       case "E5":  
         MartaStation=" East Lake ";
         StationURL = "https://itsmarta.com/East-Lake.aspx";
+        StationGPS = "";
         break;
       case "E6":  
         MartaStation=" Decatur ";
         StationURL = "https://itsmarta.com/Decatur.aspx";
+        StationGPS = "33.77462519237718,-84.29615194824973";
         break;
       case "E7":  
         MartaStation=" Avondale ";
         StationURL = "https://itsmarta.com/Avondale.aspx";
+        StationGPS = "33.77519,-84.28214";
         break;
       case "E8":  
         MartaStation=" Kensington ";
         StationURL = "https://itsmarta.com/Kensington.aspx";
+        StationGPS = "33.77265,-84.25205";
         break;
       case "E9":  
         MartaStation=" Indian Creek ";
         StationURL = "https://itsmarta.com/Indian-Creek.aspx";
+        StationGPS = "";
         break;
       case "W0":  
         MartaStation=" Five Points ";
         StationURL = "https://itsmarta.com/Five-Points.aspx";
+        StationGPS = "33.75392023598456,-84.39162662288796";
         break;
       case "W1":  
         MartaStation=" Dome / G W C C / Philips / C N N ";
         StationURL = "https://itsmarta.com/Omni.aspx";
+        StationGPS = "";
         break;
       case "W2":  
         MartaStation=" Vine City ";
         StationURL = "https://itsmarta.com/Vine-City.aspx";
+        StationGPS = "";
         break;
       case "W3":  
         MartaStation=" Ashby ";
         StationURL = "https://itsmarta.com/Ashby.aspx";
+        StationGPS = "";
         break;
       case "W4":  
         MartaStation=" West Lake ";
         StationURL = "https://itsmarta.com/West-Lake.aspx";
+        StationGPS = "";
         break;
       case "W5":  
         MartaStation=" Hamilton E Holmes ";
         StationURL = "https://itsmarta.com/Hamilton-E-Holmes.aspx";
+        StationGPS = "";
         break;
       case "P4":  
         MartaStation=" Bankhead ";
         StationURL = "https://itsmarta.com/Bankhead.aspx";
+        StationGPS = "";
         break;
       case "S1":  
         MartaStation=" Garnett ";
         StationURL = "https://itsmarta.com/Garnett.aspx";
+        StationGPS = "";
         break;
       case "S2":  
         MartaStation=" West End ";
         StationURL = "https://itsmarta.com/West-End.aspx";
+        StationGPS = "";
         break;
       case "S3":  
         MartaStation=" Oakland City ";
         StationURL = "https://itsmarta.com/Oakland-City.aspx";
+        StationGPS = "";
         break;
       case "S4":  
         MartaStation=" Lakewood / Fort Mcpherson ";
         StationURL = "https://itsmarta.com/Lakewood.aspx";
+        StationGPS = "";
         break;
       case "S5":  
         MartaStation=" East Point ";
         StationURL = "https://itsmarta.com/East-Point.aspx";
+        StationGPS = "";
         break;
       case "S6":  
         MartaStation=" College Park ";
         StationURL = "https://itsmarta.com/College-Park.aspx";
+        StationGPS = "";
         break;
       case "S7":  
         MartaStation=" Airport ";
         StationURL = "https://itsmarta.com/Airport.aspx";
+        StationGPS = "33.64080453587645,-84.44624731278977";
         break;
       case "N1":  
         MartaStation=" Peachtree Center ";
         StationURL = "https://itsmarta.com/Peachtree-Center.aspx";
+        StationGPS = "";
         break;
       case "N2":  
         MartaStation=" Civic Center ";
         StationURL = "https://itsmarta.com/Civic-Center.aspx";
+        StationGPS = "";
         break;
       case "N3":  
         MartaStation=" North Avenue";
         StationURL = "https://itsmarta.com/North-Ave.aspx";
+        StationGPS = "33.77313,-84.38690";
         break;
       case "N4":  
         MartaStation=" Midtown ";
         StationURL = "https://itsmarta.com/Midtown.aspx";
+        StationGPS = "";
         break;
       case "N5":  
         MartaStation=" Arts Center ";
         StationURL = "https://itsmarta.com/Arts-Center.aspx";
+        StationGPS = "";
         break;
       case "N6":  
         MartaStation=" Lindbergh Center ";
         StationURL = "https://itsmarta.com/Lindbergh.aspx";
+        StationGPS = "";
         break;
       case "N7":  
         MartaStation=" Buckhead ";
         StationURL = "https://itsmarta.com/Buckhead.aspx";
+        StationGPS = "";
         break;
       case "N8":  
         MartaStation=" Medical Center ";
         StationURL = "https://itsmarta.com/Medical-Center.aspx";
+        StationGPS = "";
         break;
       case "N9":  
         MartaStation=" Dunwoody ";
         StationURL = "https://itsmarta.com/Dunwoody.aspx";
+        StationGPS = "";
         break;
       case "N10":  
         MartaStation=" Sandy Springs ";
         StationURL = "https://itsmarta.com/Sandy-Springs.aspx";
+        StationGPS = "";
         break;
       case "N11":  
         MartaStation=" North Springs ";
         StationURL = "https://itsmarta.com/North-Springs.aspx";
+        StationGPS = "";
         break;
       case "NE7":  
         MartaStation=" Lenox ";
         StationURL = "https://itsmarta.com/Lenox.aspx";
+        StationGPS = "";
         break;
       case "NE8":  
         MartaStation=" Brookhaven / Oglethorpe ";
         StationURL = "https://itsmarta.com/Brookhaven.aspx";
+        StationGPS = "";
         break;
       case "NE9":  
         MartaStation=" Chamblee ";
         StationURL = "https://itsmarta.com/Chamblee.aspx";
+        StationGPS = "";
         break;
       case "NE10":  
         MartaStation=" Doraville ";
         StationURL = "https://itsmarta.com/Doraville.aspx";
+        StationGPS = "";
         break;
        case "":  
         MartaStation=" ERROR: No Station Provided ";
         StationURL = "";
+        StationGPS = "";
         break;
       default: 
         MartaStation=" ERROR: "+station;
         StationURL = "";
+        StationGPS = "";
         break;
     } //*/
     
